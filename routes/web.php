99cceb30fe02ce26e71ed9862dcbddb300fb00eb
yu_sato <yu_sato@net-nsi.co.jp>
2024-04-30 07:03:33 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;   // 追加
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// 追加
+Route::get('/', [TasksController::class, 'index']);
+Route::resource('tasks', TasksController::class);
 
 require __DIR__.'/auth.php';
